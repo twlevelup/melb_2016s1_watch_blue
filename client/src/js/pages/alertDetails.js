99018cc32131feda3537cd8f05e3eef0794921cf
Lina <linachan.nz@gmail.com>
2016-04-12 20:27:54 +1000

    Basic pulling data.json implemented

--- client/src/js/pages/alertDetails.js
+++ client/src/js/pages/alertDetails.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var PageView = require('../framework/page');
-
 var AlertsView = require('../views/alertDetails');
 
 var AlertsView = PageView.extend({
@@ -9,14 +8,15 @@ var AlertsView = PageView.extend({
   id: 'alertDetails',
 
   alertDefinition: {
-    title: "Tropical Cyclone",
-    location: "50km from your current location",
-    time: "Tuesday 22 March at 8pm",
-    severity: "High",
-    warningDescription: "Tropical Cyclone Nancy is a Cat 4 cyclone. \
-    It is currently heading to Brisbane, expected to touch down at \
-    11pm tonight."
-  },
+            "time": "",
+            "date": "",
+            "type": "",
+            "severity": "",
+            "location": "",
+            "longtitude":"",
+            "latitude":"",
+            "description": ""
+        },
 
   template: require('../../templates/pages/alertDetails.hbs'),
 
@@ -29,6 +29,15 @@ var AlertsView = PageView.extend({
   initialize: function() {
     //this.contactsCollection = new ContactsCollection();
     //this.seedContacts();
+    var thisHandle = this;
+    var feed_data = $.getJSON( "/images/feed.json", {}, function(data) {
+      thisHandle.alertDefinition = data["weather"][0];
+      thisHandle.render();
+
+    } );
+    console.log(feed_data.responseText);
+
+    console.log("initialize");
     this.render();
   },
 
@@ -37,6 +46,25 @@ var AlertsView = PageView.extend({
   },
 
   render: function() {
+<<<<<<< c652fcf4cadac6f532885e1ce4da54543dd7c9d7
+||||||| merged common ancestors
+/*
+<h1 class="title">{{title}}</h1>
+<h3 class="location">{{location}}</h3>
+<h3 class="time">{{time}}</h3>
+<h3 class="severity">{{severity}}</h3>
+<p class="warning-description">{{warningDescription}}</p>
+*/
+=======
+/*
+<h1 class="title">{{title}}</h1>
+<h3 class="location">{{location}}</h3>
+<h3 class="time">{{time}}</h3>
+<h3 class="severity">{{severity}}</h3>
+<p class="warning-description">{{warningDescription}}</p>
+*/
+    console.log("initialize 2");
+>>>>>>> Basic pulling data.json implemented
     this.$el.html(this.template(this.alertDefinition));
 
     var contactsHTML = document.createDocumentFragment();
@@ -48,7 +76,37 @@ var AlertsView = PageView.extend({
     this.$el.find('ul').html(contactsHTML);
 
     return this;
+<<<<<<< c652fcf4cadac6f532885e1ce4da54543dd7c9d7
   }
 });
+||||||| merged common ancestors
+  },
+
+  createContactHTML: function(contact) {
+      var view = new ContactView({
+        model: contact
+      });
+      return view.render().el;
+    }
+
+});
+=======
+  },
+
+  createContactHTML: function(contact) {
+      var view = new ContactView({
+        model: contact
+      });
+      return view.render().el;
+    },
+
+  get_data: function(data) {
+
+      this.render();
+  }
+}
+
+);
+>>>>>>> Basic pulling data.json implemented
 
 module.exports = new AlertsView();
